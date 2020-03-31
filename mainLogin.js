08dc236efed52b8abdb57d6e6f9930dc049f3d58
mcleesmorgan <mclees.morgan@gmail.com>
2020-03-30 23:11:33 -0400

    final changes for 3/30
    login/logout works now
    dashboard appears and you can click back and forth between home and myhospital - which changes depending on whether hospital owner or not

--- mainLogin.js
+++ mainLogin.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var User = require('./User.js');
 
+var currentUser = null;
+
 app.use('/create', (req, res) => {
     
     var newPerson = new User({
@@ -35,65 +37,44 @@ app.use('/checkLogin', (req, res) => {
         queryUser = {"username" : req.body.username};
         if (req.body.password) {
             queryUser = {"password" : req.body.password};
-        } else {
-            changeVisibility();
-        }
-    } else {
-        changeVisibility();
-    }
+        } 
+    } 
     
     User.find(queryUser, (err, users) => {
         if (err) {
             console.log('uh oh' + err);
             res.json({});
         } else if (users.length == 0) {
-            res.json({});
+            res.redirect('/');
         } else {
             var returnArray = [];
             users.forEach( (user) => {
                 returnArray.push(user);
             });
             
-            console.log('made it here');
             if (returnArray) {
-                console.log('logged in');
-                //changePage();
+                currentUser = returnArray[0];
                 res.render('differentDashboard', {user: returnArray});
             } else {
-                console.log('failed');
-                res.redirect('/public/login.html');
+                res.redirect('/');
             }
         }
     });
 });
 
-/*function changePage() {
-    window.redirect('/dashboard.html');
-}
-
-function changeVisibility() {
-    window.redirect('/login.html');
-    var errorText = document.getElementById('ifWrongLogin');
-    errorText.setAttribute('style', 'visibility: visible');
-}*/
-
 app.get('/home', function (req, res) {
-    res.redirect('differentDashboard.ejs');
+    res.render('differentDashboard', {user: currentUser});
 });
 
 app.get('/myHospital', function (req, res) {
-    res.redirect('myHospital.ejs');
-});
-
-app.get('/logout', function (req, res) {
-    res.redirect('../public/login.html');
+    res.render('myHospital', {user: currentUser});
 });
 
 app.use('/public', express.static('public'));
 
-app.use('/', (req, res) => {res.redirect('/public/login.html');
-                           console.log('here');
-});
+app.use('/styles', express.static('styles'));
+
+app.use('/', (req, res) => {res.redirect('/public/login.html');});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
