9b98f088be362ee813367546ad234eb988f5378f
mcleesmorgan <mcleesm@seas.upenn.edu>
2020-03-30 23:15:15 -0400

    Delete users.js
    this is out of date now

--- Users.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-
-//This is gonna be something different depending on where the database is located. 
-mongoose.connect('mongodb://localhost:27017/myDatabase');
-
-var Schema = mongoose.Schema;
